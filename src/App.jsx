79e19dbe656acc8f8ccba36a0dd3a1026d5b521d
Yu-326-ta <yoshinagayuuta@outlook.jp>
2022-09-21 20:45:00 +0900

    ユーザー詳細画面の作成

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,16 @@ import Home from './pages/Home';
 import Form from './pages/Form';
 import Rank from './pages/Rank';
 import { useEffect, useState } from 'react';
-import { getPosts } from './api/jsonplaceholder';
 import Detail, { loader as detailLoader } from './pages/Detail';
 
 
 function App() {
-  const [posts, setPosts] = useState([])
-
-  // ページの初回レンダリング時に実行
-  useEffect(() => {
-    const initPosts = async() => {
-      const fetchedPosts = await getPosts()
-      setPosts(fetchedPosts)
-    }
-    initPosts()
-  }, [])
 
   // ルーティング
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home posts={posts} />,
+      element: <Home />,
     },
     {
       path: "/form",
